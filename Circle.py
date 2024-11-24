b191c6eeed38e2f5fb2ac3ff32d8ba003ea4200f
craigchihombe <achihombe@gmail.com>
2024-11-24 10:00:18 +0200

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,7 @@
+#Circle
+print("A circle is circular object. \n It has a circumfrance which is the edge like part of it. \n It also has a diameter which is the half of the circle. \n The radius is half of the diameter.\n *All these parts work together as a family.*")
+print("That's a circle")
+print("Hope you enjoyed my summary of a circle")
+#Area and perimeter of a circle
+print("The area of a circle is pi times the radius squared (A = π r²).")
+print("The perimeter is calculated by adding all of the numbers provided.")
